d50be69043f7054eefd6f3acbe671f188a02fc3c
Francesco Riccardo Di Gioia <107129520+francescordigioia@users.noreply.github.com>
2026-04-08 16:15:17 +0200

    add a property that needs to be $bindable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,15 @@
 <script>
     import ClicksCounter from "$lib/components/ClicksCounter.svelte";
+
+    let count = $state(5)
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<ClicksCounter initialCount={0}/>
+<ClicksCounter initialCount={count}/>
+
+<p>count from parent is {count}</p>
 
 <style>
     h1 {
