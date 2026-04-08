d39c2505bb814d619bb8b915e34e3638b70a4cb3
Tommaso Negri <me@tommasongr.com>
2026-04-08 17:28:59 +0200

    use $effect to update canvas

--- src/lib/components/CanvasSquare.svelte
+++ src/lib/components/CanvasSquare.svelte
@@ -1,12 +1,10 @@
 <script>
-    import { onMount } from "svelte"
-
     let size = $state(50)
     let color = $state("#ff3e00")
 
     let canvas
 
-    onMount(() => {
+    $effect(() => {
         const context = canvas.getContext("2d")
         context.clearRect(0, 0, canvas.width, canvas.height)
 
