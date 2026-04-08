f246e39ac2225e10020521bd94dd49b91c33d6fd
Tommaso Negri <me@tommasongr.com>
2026-04-08 15:22:28 +0200

    add property that needs to be $derived

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     let count = $state(0)
+    let doubled = count * 2
 
     console.log("hello world")
 </script>
@@ -10,6 +11,8 @@
 <button onclick={() => count++}>
     Clicks: {count}
 </button>
+
+<span>doubled is {doubled}</span>
 
 <style>
     h1 {
