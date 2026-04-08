32f330c92f6490ba3011cf20183ad26d34dd74de
Francesco Riccardo Di Gioia <107129520+francescordigioia@users.noreply.github.com>
2026-04-08 16:22:38 +0200

    make property $bindable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<ClicksCounter initialCount={count}/>
+<ClicksCounter bind:initialCount={count}/>
 
 <p>count from parent is {count}</p>
 
