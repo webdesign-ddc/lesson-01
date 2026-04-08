1ba9241af5231cb9ff96e8ba51883eae0c59af73
Francesco Riccardo Di Gioia <107129520+francescordigioia@users.noreply.github.com>
2026-04-08 15:49:31 +0200

    customise components with $props

--- src/lib/components/ClicksCounter.svelte
+++ src/lib/components/ClicksCounter.svelte
@@ -1,5 +1,7 @@
 <script>
-    let count = $state(0)
+    let props = $props()
+
+    let count = $state(props.initialCount)
     let doubled = $derived(count * 2)
 </script>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<ClicksCounter />
+<ClicksCounter initialCount={0}/>
 
 <style>
     h1 {
