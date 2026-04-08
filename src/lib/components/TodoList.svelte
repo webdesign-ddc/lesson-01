a8e42ff885b7ceeb21d9916ce610414c00200caf
Francesco Riccardo Di Gioia <107129520+francescordigioia@users.noreply.github.com>
2026-04-08 17:58:27 +0200

    improve todos with deep state binding

--- src/lib/components/TodoList.svelte
+++ src/lib/components/TodoList.svelte
@@ -1,7 +1,7 @@
 <script>
     let todos = $state([
         { title: "Assignment 1", done: false},
-        { title: "Assignment 2", done: false},
+        { title: "Assignment 2", done: true},
         { title: "Assignment 3", done: false},
     ])
 </script>
@@ -10,6 +10,9 @@
 
 <ul>
     {#each todos as todo}
-        <li>{todo.title}</li>
+        <li>
+            <input type="checkbox" bind:checked={todo.done}>
+            {todo.title}
+        </li>
     {/each}
 </ul>
