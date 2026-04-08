b9ec644d8e6e751fdc21de9d211a51fc3aa40d60
Tommaso Negri <me@tommasongr.com>
2026-04-08 14:59:01 +0200

    add style tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style>
+    h1 {
+        color: deepskyblue;
+    }
+
+    p {
+        color: deeppink;
+    }
+</style>
